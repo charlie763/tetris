b86f95ded3145f8024e68082d6a786025f094f16
charlie763 <cwisoff@gmail.com>
2020-09-04 08:57:53 -0600

    abstract out parent Piece class from lPiece class

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,4 @@
+//change to something like BOARD = Array(26).map(()=>Array(12));
 const BOARD = [
   [0,0,0,0,0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0,0,0,0],
@@ -28,20 +29,6 @@ const BOARD = [
   [0,0,0,0,0,0,0,0,0,0,0,0]
 ];
 
-class Piece{
-  //should consist of 4 cells
-  //should have children classes for the different types of pieces
-  constructor(){
-    this.cells = this.makeCells();
-    this.color = "red";
-    // this.color = setRandomColor();
-  }
-
-  makeCells(){
-    return [new Cell(6,0), new Cell(6,1), new Cell(6,2), new Cell(6,3)];
-  }
-}
-
 class Cell{
   //should have x and y position properties
   constructor(xPos, yPos){
@@ -50,10 +37,32 @@ class Cell{
   }
 }
 
+class Piece{
+  //should consist of 4 cells
+  //should have children classes for the different types of pieces
+  constructor(){
+    this.color = "red";
+    // this.color = setRandomColor();
+  }
+  
+}
+
+class lPiece extends Piece {
+  constructor(){
+    super();
+    this.cells = this.makeCells();
+  }
+
+  makeCells(){ //change to static method?
+    return [new Cell(6,0), new Cell(6,1), new Cell(6,2), new Cell(6,3)];
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', ()=>{
   const board = document.querySelector('.board');
   displayNewBoard();
-  const testPiece = new Piece();
+  const testPiece = new lPiece();
   testPiece.makeCells();
   addPiece(testPiece);
 
@@ -70,21 +79,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
   }
 
   function addPiece(piece){
-    // trackPiece(piece);
-    // displayPiece(piece);
     for (const cell of piece.cells){
       BOARD[cell.yPos][cell.xPos] = cell;
       displayCell = document.getElementById(`x${cell.xPos}y${cell.yPos}`);
       displayCell.style.backgroundColor = piece.color;
     }
   }
-
-  // function trackPiece(piece){
-   
-  // }
-
-  // function displayPiece(piece){
-
-  // }
 });
 
