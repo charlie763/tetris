4707416b636ef7fb4e4075f799143f676325e787
charlie763 <cwisoff@gmail.com>
2020-09-09 13:34:31 -0600

    persist completed game upon request

--- backend/app/controllers/completed_games_controller.rb
+++ backend/app/controllers/completed_games_controller.rb
@@ -3,5 +3,12 @@ class CompletedGamesController < ApplicationController
   end
 
   def create
+    game = CompletedGame.create(game_params)
+    render json: {message: "game recieved"}
+  end
+
+  private
+  def game_params
+    params.require(:completed_game).permit(:score, :user_id)
   end
 end
