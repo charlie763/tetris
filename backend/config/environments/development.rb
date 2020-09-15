0da8858123fd4149e414bfe2cd0c45cbaf2feb83
charlie763 <cwisoff@gmail.com>
2020-09-15 14:59:20 -0600

    clean up code

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -2,11 +2,8 @@
 
 Rails.application.configure do
   config.cache_classes = false
-
   config.eager_load = false
-
   config.consider_all_requests_local = true
-
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -19,16 +16,10 @@ Rails.application.configure do
   end
 
   config.active_storage.service = :local
-
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
-
   config.active_support.deprecation = :log
-
   config.active_record.migration_error = :page_load
-
   config.active_record.verbose_query_logs = true
-
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
